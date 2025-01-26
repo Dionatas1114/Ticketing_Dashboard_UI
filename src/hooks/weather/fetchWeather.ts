ca80d11fcbe4ca4a3b4210971358e179551c8bf4
pc <sem.dionatas@gmail.com>
2025-01-25 23:34:17 -0300

    remove env var hardcoded

--- src/hooks/weather/fetchWeather.ts
+++ src/hooks/weather/fetchWeather.ts
@@ -1,6 +1,4 @@
 export const api = {
-  // key: 'd27b3234554da73a4283faf15be40cc1',
-  // baseURL: 'https://api.openweathermap.org/data/2.5',
   city: import.meta.env.VITE_APP_WEATHER_API_CITY || 'Parobe',
   key: import.meta.env.VITE_APP_WEATHER_API_KEY,
   baseURL: import.meta.env.VITE_APP_WEATHER_API_URL,
