1e044230233330245d453c084f6eab2b115af2ab
pc <sem.dionatas@gmail.com>
2025-01-27 20:05:48 -0300

    remove sanitizeUserName

--- src/components/inputs/inputUserName/index.tsx
+++ src/components/inputs/inputUserName/index.tsx
@@ -1,10 +1,10 @@
 import { TextField, TextFieldProps } from '@mui/material';
 
-const sanitizeUserName = (event: React.ChangeEvent<HTMLInputElement>) => {
-  let value = event.target.value;
-  value = value.replace(/[^a-zA-Z ]/g, '').trimStart(); // Remove espaços no início e caracteres inválidos
-  event.target.value = value; // Atualiza o valor no input que será retornado para o componente Pai
-};
+// const sanitizeUserName = (event: React.ChangeEvent<HTMLInputElement>) => {
+// let value = event.target.value;
+// value = value.replace(/[^a-zA-Z ]/g, '').trimStart(); // Remove espaços no início e caracteres inválidos
+// event.target.value = value; // Atualiza o valor no input que será retornado para o componente Pai
+// };
 
 const InputUserName = (props: TextFieldProps) => (
   <TextField
@@ -12,7 +12,7 @@ const InputUserName = (props: TextFieldProps) => (
     name="name"
     autoComplete="name"
     fullWidth
-    onInput={sanitizeUserName}
+    // onInput={sanitizeUserName}
     {...props}
     sx={{
       '& .MuiFormHelperText-root': {
